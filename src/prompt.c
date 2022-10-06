9c275fa3720b57d1253c4cbcfb7b4e925b5eb27b
Kevin Brousse <kbrousse@2D5.42angouleme.fr>
2022-10-06 10:22:59 +0200

    Pushed

--- src/prompt.c
+++ src/prompt.c
@@ -24,17 +24,22 @@ void	ft_signal(int sig)
 void	cmd_prompt(char **env)
 {
 	char	*user_input;
-(void)env;
+
 	while (1)
 	{
 		user_input = readline("minishell> ");
-		(void)user_input;
+		if (!user_input)
+		{
+			rl_clear_history();
+			write(1, "exit\n", 4);
+			exit(0);
+		}
 		if (ft_strncmp(user_input, "exit", 4) == 0)
 		{
 			rl_clear_history();
 			exit(0);
 		}
-		if (user_input)
+		if (ft_strlen(user_input) > 0)
 			add_history(user_input);
 		user_input = filter_cmd_line(user_input, env);
 		ft_printf("%s\n", user_input);
