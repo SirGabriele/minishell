2eb3b67ef47d18b142ccbf0284f88bdf9e571afe
Jordan Sauvain <jsauvain@1G7.42angouleme.fr>
2022-11-19 17:03:44 +0100

    minishell_fork

---
+++ src/parsing/get_redirections.c
@@ -0,0 +1,6 @@
+#include "../../includes/minishell.h"
+
+t_context_ms	*get_redirections(t_token_ms *tokens, t_context_ms *cmd_line)
+{
+	
+}
--- src/parsing/structure_cmd_lst.c
+++ src/parsing/structure_cmd_lst.c
@@ -16,12 +16,6 @@ void	print_pipelines(t_context_ms *cmd_line)
 	printf("\n");
 }
 
-/*
-int	get_redirections(t_token_ms *tokens, t_context_ms *cmd_line)
-{
-
-}
-*/
 t_context_ms	*structure_cmd_lst(t_token_ms *tokens)
 {
 	t_context_ms	*cmd_lst;
@@ -32,11 +26,9 @@ t_context_ms	*structure_cmd_lst(t_token_ms *tokens)
 	cmd_lst = get_all_pipelines(tokens, cmd_lst);
 	if (!cmd_lst)
 		return (NULL);
-	print_pipelines(cmd_lst); //a supprimer
-	/*else if (get_redirections(tokens, cmd_line) == -1)
-	{
-		free_context(cmd_line);
+	//print_pipelines(cmd_lst); //a supprimer
+	cmd_lst = get_redirections(tokens, cmd_line);
+	if (!cmd_lst)
 		return (NULL);
-	}*/
 	return (cmd_lst);
 }
