952a8973154ae36dfb738c765b69dbef1464a99f
Kevin Brousse <kbrousse@2D5.42angouleme.fr>
2023-01-10 13:56:03 +0100

    Cd should be all good

--- src/builtins/ft_cd_bonus.c
+++ src/builtins/ft_cd_bonus.c
@@ -1,5 +1,15 @@
 #include "../../includes/minishell.h"
 
+static int	count_args(char **content)
+{
+	int	i;
+
+	i = 1;
+	while (content[i])
+		i++;
+	return (i);
+}
+
 static void	change_pwd_and_oldpwd(t_env_ms *env_ll)
 {
 	t_env_ms	*old_pwd;
@@ -20,60 +30,53 @@ static void	change_pwd_and_oldpwd(t_env_ms *env_ll)
 	pwd->value = post_cd_pwd;
 }
 
-static int	count_args(char **content)
-{
-	int	i;
-
-	i = 1;
-	while (content[i])
-		i++;
-	return (i);
-}
-
-static int	identify_error(char *content, t_env_ms *env_ll)//EFAULT EIO ELOOP
+/*static int	identify_error(char *content, t_env_ms *env_ll)
 {
 	char	*error;
 
 	error = ft_strjoin("minishell: cd: ", content);
 	if (!error)
 		perror(NULL);
-	if (!error)
-		perror(NULL);
-	if (content[0] != '-')
+	else
 	{
 		perror(error);
 		set_exit_code(env_ll, 1);
 	}
-	else
-	{
-		ft_printf_fd(2, "minishell: cd: %s: invalid option\n", content);
-		set_exit_code(env_ll, 2);
-	}
 	free(error);
 	return (1);
+}*/
+
+static int	execute_cd(char *content, t_env_ms *env_ll)
+{
+	char	*error;
+	int		exit_status;
+
+	error = ft_strjoin("minishell: cd: ", content);
+	if (!error)
+		perror(NULL);
+	exit_status = chdir(content);
+	if (exit_status)
+	{
+		perror(error);
+		set_exit_code(env_ll, 1);
+		free(error);
+		return (1);
+	}
+	change_pwd_and_oldpwd(env_ll);
+	set_exit_code(env_ll, 0);
+	free(error);
+	return (0);
 }
 
 int	ft_cd(char **content, t_env_ms *env_ll)
 {
-	int	exit_status;
 	int	nb_args;
+	int	ret;
 
+	ret = 0;
 	nb_args = count_args(content);
 	if (nb_args == 1)
-	{
-		exit_status = chdir(content[0]);
-		if (exit_status)
-		{
-			identify_error(*content, env_ll);
-			return (1);
-		}
-		else
-		{
-			change_pwd_and_oldpwd(env_ll);
-			set_exit_code(env_ll, 0);
-			return (0);
-		}
-	}
+		ret = execute_cd(content[0], env_ll);
 	else
 	{
 		if (!nb_args)
@@ -83,5 +86,5 @@ int	ft_cd(char **content, t_env_ms *env_ll)
 		set_exit_code(env_ll, 1);
 		return (1);
 	}
-	return (0);
+	return (ret);
 }
